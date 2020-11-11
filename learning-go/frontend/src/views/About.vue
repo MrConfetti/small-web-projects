c3c3db9b2f14f082078e5ef35fd74ff32b2b27ad
Radu Catarambol <catarambolradu@gmail.com>
2020-11-11 18:21:57 +0200

    Added CORS to backend; connected backend with frontend via docker-compose

--- learning-go/frontend/src/views/About.vue
+++ learning-go/frontend/src/views/About.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p @click="callApi">Click me!</p>
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'About',
+  methods: {
+    callApi() {
+      axios
+      .get('http://localhost:8080/books')
+      .then(response => {
+        console.log(response)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+      .finally()
+    }
+  }
+}
+</script>
--- learning-go/server/main.go
+++ learning-go/server/main.go
@@ -7,13 +7,22 @@ import (
 	"learning-go/main/models"
 )
 
+func cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Add("Access-Control-Allow-Origin", "*")
+		c.Next()
+	}
+}
+
 func main() {
 	r := gin.Default()
 
 	models.ConnectDatabase()
 
+	r.Use(cors())
+
 	r.GET("/books", controllers.FindBooks)
 	r.POST("/books", controllers.CreateBook)
 
-	r.Run()
+	r.Run(":8080")
 }
